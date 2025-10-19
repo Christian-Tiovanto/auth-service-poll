07a4f26088b305e66f64c05128819d82ac5e68b2
Christian Tiovanto <christiantiovanto1@gmail.com>
2025-10-19 15:43:42 +0000

    chore: fix default port to use .env or default

--- src/main.ts
+++ src/main.ts
@@ -5,37 +5,12 @@ import { UserSeeder } from './seeder/user.seeder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3005;
+
   const seeder = app.get(UserSeeder);
   await seeder.run();
 
-  // const environment = process.env.NODE_ENV || 'DEVELOPMENT';
-  // const domainUrl = process.env.DOMAIN_URL || 'http://localhost:5173';
-
-  // let corsOptions = {};
-
-  // if (environment === 'PRODUCTION') {
-  //   corsOptions = {
-  //     origin: domainUrl, // hanya domain produksi yang diizinkan
-  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //     allowedHeaders: 'Content-Type, Accept, Authorization',
-  //     credentials: true, // jika pakai cookie/token
-  //   };
-  // } else {
-  //   corsOptions = {
-  //     origin: true, // izinkan semua origin saat development
-  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //     allowedHeaders: 'Content-Type, Accept, Authorization',
-  //     credentials: true,
-  //   };
-  // }
-
   app.enableCors();
-  // app.enableCors({
-  //   origin: '*', // Izinkan semua origin
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   allowedHeaders: 'Content-Type, Accept, Authorization',
-  //   credentials: true, // Jika menggunakan cookies atau token
-  // });
 
   const config = new DocumentBuilder()
     .setTitle('Endpoint Penitipan Barang')
@@ -46,6 +21,6 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, documentFactory);
-  await app.listen(8000, '0.0.0.0'); // <-- PENTING BANGET!
+  await app.listen(PORT, '0.0.0.0'); // <-- PENTING BANGET!
 }
 bootstrap();
